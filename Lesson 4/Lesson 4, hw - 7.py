9ac0765e72a16e5cf64c75bcd8d6672944ddb875
Vadim <rik_for_all@Mail.ru>
2020-09-24 20:39:27 +0600

    mistake in homework 7

--- Lesson 4/Lesson 4, hw - 7.py
+++ Lesson 4/Lesson 4, hw - 7.py
@@ -1,7 +1,7 @@
 def fact(n):
     num = 1
     for el in range(1, n + 1):
-        num = num + el
+        num = num * el
         yield f'{el}! = {num}'
 n = int(input("Введите число для нахождения его факториала "))
 for el in fact(n):
